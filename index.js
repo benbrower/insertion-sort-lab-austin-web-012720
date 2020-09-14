aa6cf9b040299c4a91076dc64c806e25daf11cc2
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 19:59:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function findMinAndReturn(arr) {
+  let min = arr[0];
+  let index = 0;
+  for (i = 0; i < arr.length; i++)
+}
+
+function selectionSort(arr){
+  let sorted = [];
+  while (arr.length){
+    sorted.push(findMinAndReturn(arr));
+  }
+  return sorted;
+}
