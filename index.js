b0ff6674743268ac33695f71855b64ba170cf6f7
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 20:01:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function findMinAndReturn(arr) {
   let min = arr[0];
   let index = 0;
-  for (i = 0; i < arr.length; i++)
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+      index = i
+    }
+  }
+  return arr.slice(index, 1);
 }
 
 function selectionSort(arr){
