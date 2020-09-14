270bdd0be7bd12bbfb0b3df47d43d2d9b92448d0
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 20:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function findMinAndReturn(arr) {
   let min = arr[0];
   let index = 0;
-  for (i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++){
     if (arr[i] < min){
       min = arr[i];
       index = i;
